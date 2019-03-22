0fbc280a5b0fcb6c662792a18a64cd6dc1cb3ebb
neil patel <neil@myindianweddingapp.com>
2019-03-22 13:38:44 +0000

    Setup moduules for budget and UI controllers

---
+++ app.js
@@ -0,0 +1,13 @@
+var budgetController = (function() {
+    
+})();
+
+
+var UIController = (function() {
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+
+})(budgetController, UIController);
