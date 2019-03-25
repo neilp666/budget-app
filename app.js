8d12c42f437f0e60052c8f2b06752c34151be79e
neil patel <neil@myindianweddingapp.com>
2019-03-25 15:54:08 +0000

    Set up event listeners on enter button

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+// Budget Controller
 var budgetController = (function() {
     
 })();
 
+// UI Controller
 
 var UIController = (function() {
 
 })();
 
+
+// Global APP Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function() {
+        console.log('It works');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIController);
