b1cf28c6d6373b9dfb5a8eb96f8b2e3c85baa059
neil patel <neil@myindianweddingapp.com>
2019-04-01 20:17:46 +0100

    Updated budget controller to total the income and expenses

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ var budgetController = (function() {
         this.description = description;
         this.value = value;
     };
+
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
     
     var data = {
         allItems: {
@@ -21,7 +29,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -42,6 +52,31 @@ var budgetController = (function() {
 
             data.allItems[type].push(newItem);
             return newItem;
+        },
+        calculateBudget: function() {
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            data.budget = data.totals.inc - data.totals.exp;
+
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
+        },
+
+        testing: function() {
+            console.log(data);
         }
     };
 })();
@@ -125,8 +160,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var updateBudget = function() {
-        
-    }
+        budgetCtrl.calculateBudget();
+        var budget = budgetCtrl.getBudget();
+        console.log(budget);
+    };
 
     var ctrlAddItem = function() {
          var input, newItem;
