84a528d437cafc1cdb138f66e274f5fb26459017
neil patel <neil@myindianweddingapp.com>
2019-04-01 15:52:59 +0100

    Added function if user does not add description or value in input fields

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -124,16 +124,26 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     };
 
+    var updateBudget = function() {
+        
+    }
+
     var ctrlAddItem = function() {
          var input, newItem;
 
          input = UICtrl.getInput();
-         
-         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-         UICtrl.addListItem(newItem, input.type);
+         if (input.description !== "" && !isNaN(input.value) && input.value > 0 ) {
 
-         UICtrl.clearFields();
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+            UICtrl.addListItem(newItem, input.type);
+
+            UICtrl.clearFields();
+
+            updateBudget();
+
+         }
          
     };
 
