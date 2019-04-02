77251bec4830cbfd1d2c3d8dc1e16b4012beac9a
neil patel <neil@myindianweddingapp.com>
2019-04-02 14:16:30 +0100

    Updated the UI to show totals of incomes, expenses and percentages

--- app.js
+++ app.js
@@ -91,7 +91,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -137,6 +141,19 @@ var UIController = (function() {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -161,8 +178,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var updateBudget = function() {
         budgetCtrl.calculateBudget();
+
         var budget = budgetCtrl.getBudget();
-        console.log(budget);
+
+        UICtrl.displayBudget(budget);
     };
 
     var ctrlAddItem = function() {
@@ -179,14 +198,19 @@ var controller = (function(budgetCtrl, UICtrl) {
             UICtrl.clearFields();
 
             updateBudget();
-
          }
          
     };
 
     return {
         init: function() {
-            console.log('Application has started.')
+            console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
