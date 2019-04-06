d507e8b87fb9afd9aafce2d9597b804edb53b51b
neil patel <neil@myindianweddingapp.com>
2019-04-06 11:07:37 +0100

    Updated the percentages on the UI

--- app.js
+++ app.js
@@ -136,7 +136,8 @@ var UIController = (function() {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     };
 
     return {
@@ -201,6 +202,26 @@ var UIController = (function() {
             }
         },
 
+        displayPercentages: function(percentages) {
+            
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current, index) {
+
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -237,7 +258,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updatePercentages = function() {
         budgetCtrl.calculatePercentages();
         var percentages = budgetCtrl.getPercentages();
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
 
     var ctrlAddItem = function() {
